7c2950521b1337499b53c60e7f9190b54a4896f4
s095339 <s095339@gmail.com>
2024-01-11 13:18:21 -0500

    [modify] write start code for dma

--- testbench/counter_la_fir/fir.c
+++ testbench/counter_la_fir/fir.c
@@ -1,30 +1,35 @@
 #include "fir.h"
+#include <stdint.h> 
 
-void __attribute__ ( ( section ( ".mprjram" ) ) ) initfir() {
-	for(int i=0; i<N; i++) {
-		inputbuffer[i] = 0;
-		outputsignal[i] = 0;
-	}
-}
 
-int __attribute__ ( ( section ( ".mprjram" ) ) ) firfilter(int inputsample){
-	for(int i=N-1; i>0; i--){
-		inputbuffer[i] = inputbuffer[i-1];
-	}
-	inputbuffer[0] = inputsample;
+
+void __attribute__ ( ( section ( ".mprjram" ) ) ) fir(){
+	//fir accelerater================================
 	
-	int outputsample = 0;
-	for(int i=0; i<N; i++){
-		outputsample += taps[i]*inputbuffer[i];
-	}
-	return outputsample;
-}
+	//step 1 send parameter to accelerater (taps)
+	for(int i=0;i<N;i++){
+        send_wb(WB_FIR_TAP_START + i*4, taps[i]);
+    }
+	//step 2 allocate input and output buffer
+	for(int i=0;i<NI;i++){
+        inputsignal[i] = i;
+    }
+	start_dma((uint32_t) INDATA_ADR, NI,OUTDATA_ADR);
+	//step 3 start the fir accelerater
 
-int* __attribute__ ( ( section ( ".mprjram" ) ) ) fir(){
-	initfir();
-	for(int i=0; i<N; i++){
-		outputsignal[i] = firfilter(inputsignal[i]);
-	}
-	return outputsignal;
+
+	//===============================================
+
+	//return outputsignal;
 }
 		
+
+
+int __attribute__ ( ( section ( ".mprjram" ) ) ) main_func(){
+	
+	fir();
+
+	
+
+	return 0;
+}
